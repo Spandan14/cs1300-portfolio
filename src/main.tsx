842acac18c1cd006fb7e733caaf746ac1f5208f1
Spandan14 <spagoel922@gmail.com>
2025-03-10 23:37:39 -0400

    Attempt BrowserRouter solution

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { BrowserRouter as HashRouter } from 'react-router-dom'
+import { BrowserRouter as BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <HashRouter basename="/cs1300-portfolio">
+    <BrowserRouter basename="/cs1300-portfolio">
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </StrictMode>,
 )
