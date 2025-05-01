f7593571d50d5ad9442d644b0f60603005f9728e
Spandan Goel <52139195+Spandan14@users.noreply.github.com>
2025-05-01 14:32:19 -0400

    Add front page draft

---
+++ anims.js
@@ -0,0 +1,41 @@
+const texts = ["interfacing designs -> designing interfaces", "from syscalls to design calls", "pipes to cards"];
+let textIndex = 0;
+const dynamicTagline = document.getElementById("dynamic-tagline");
+
+function typeText(text, delay) {
+    const loopTyping = function() {
+        if (dynamicTagline.textContent.length < text.length) {
+            dynamicTagline.textContent += text.charAt(dynamicTagline.textContent.length);
+            setTimeout(loopTyping, delay);
+        }
+    }
+
+    loopTyping();
+}
+
+function eraseText(delay) {
+    const loopErasing = function() {
+        if (dynamicTagline.textContent.length > 0) {
+            dynamicTagline.textContent = dynamicTagline.textContent.slice(0, -1);
+            setTimeout(loopErasing, delay);
+        }
+    }
+
+    loopErasing();
+}
+
+function fullAnimation() {
+    eraseText(50);
+    setTimeout(() => {
+        textIndex = (textIndex + 1) % texts.length;
+        typeText(texts[textIndex], 100);
+        setTimeout(() => {
+            fullAnimation();
+        }, texts[textIndex].length * 100 + 1000);
+    }, texts[textIndex].length * 50 + 500);
+}
+
+setTimeout(() => {
+    fullAnimation();
+}, 1500);
+
