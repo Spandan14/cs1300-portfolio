87b43641a4c089a8c1e3028b45cd14df38131c72
Spandan Goel <52139195+Spandan14@users.noreply.github.com>
2025-05-01 16:54:56 -0400

    Finalize card setup before adding tabs

--- details.js
+++ details.js
@@ -2,3 +2,15 @@ const now = new Date();
 const formattedDate = `${now.toLocaleString('en-US', { weekday: 'short' })} ${now.toLocaleString('en-US', { month: 'short' })} ${now.getDate()} ${now.toLocaleTimeString('en-US', { hour12: false })}`;
 
 document.getElementById("login-time").innerHTML = `Last login: ${formattedDate} on ttys000`;
+
+const arrow = document.getElementById('scroll-arrow');
+
+function updateArrowVisibility() {
+    arrow.style.visibility = window.scrollY > 0 ? 'hidden' : 'visible';
+}
+
+// Hide/show on scroll
+window.addEventListener('scroll', updateArrowVisibility);
+
+// Initialize on load
+updateArrowVisibility();
